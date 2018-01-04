0191f96d01641accc89f19ba71aa2af5487d338d
sminez <innes@cocoon.life>
2018-01-04 11:13:52 +0000

    simple test of the gaussian function in main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,12 @@
+extern crate fem203;
+
+use fem203::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Gausian values ftw");
+
+    let vals = (-100..100)
+        .map(|t| gaussian(t as f64 / 100.0, 0.5, 0.1))
+        .collect::<Vec<_>>();
+    println!("{:?}", vals);
 }
